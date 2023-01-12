6f332d45573d08f10bc1904f887f25e868203dfb
brokiem <milyhqvideo@gmail.com>
2023-01-12 18:26:07 +0800

    Fix flex stuff

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center bg-gray-100 border border-black/10 rounded-md shadow-sm flex-row dark:border-gray-300/20 dark:bg-grey">
     <img class="object-cover h-[62px] w-12 rounded-l-md" :src="character.picture.replace('https://cdn.myanimelist.net/images/questionmark_23.jpg', 'https://cdn.myanimelist.net/r/42x62/images/questionmark_23.gif?s=f7dcbc4a4603d18356d3dfef8abd655c')" alt="">
-    <div class="flex flex-col justify-between pl-2 leading-normal">
+    <div class="flex-1 flex-col justify-between pl-2 pr-2 leading-normal">
       <a :href="character.link" target="_blank" class="font-normal text-[0.95rem] hover:underline text-gray-700 dark:text-gray-400">{{ character.name }}</a>
       <p class="font-normal text-xs mt-1 text-gray-700 dark:text-gray-400">{{ character.role }}</p>
     </div>
