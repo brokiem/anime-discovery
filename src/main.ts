2beb7f3fe4733c1aa697fc4848b1593f206e352d
brokiem <milyhqvideo@gmail.com>
2023-01-19 23:19:37 +0800

    Move store to different file

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,14 @@
-import { createApp } from 'vue'
-import { createStore } from 'vuex'
+import { createApp } from 'vue';
 // @ts-ignore
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register';
 
-import App from './App.vue'
-import './assets/main.css'
+import App from './App.vue';
+import './assets/main.css';
+import store from "./store";
 
-registerSW({ immediate: true })
+registerSW({ immediate: true });
 
 const app = createApp(App);
-
-const store = createStore({
-    state() {
-        return {
-            loading: null,
-            anime: {},
-            recommendations: [],
-            recommendationLoading: null,
-            hasScrollbar: false
-        }
-    },
-    mutations: {
-        setLoading(state, payload) {
-            window.document.title = "Anime Discovery";
-
-            state.loading = payload;
-        },
-        setAnime(state, payload) {
-            payload.synopsis = payload.synopsis.replaceAll('[Written by MAL Rewrite]', '').replace(/\n+$/, '').replaceAll('\n', '<br>')
-            payload.genres = payload.genres.filter(genre => genre)
-
-            state.anime = payload;
-
-            const englishTitle = payload.englishTitle.length > 0 ? ` (${payload.englishTitle})` : '';
-            window.document.title = `${payload.title}${englishTitle} | Anime Discovery`;
-            document.querySelector('meta[name="description"]')?.setAttribute("content", payload.synopsis);
-        },
-        setHasScrollbar(state, payload) {
-            state.hasScrollbar = payload;
-        },
-        setRecommendations(state, payload) {
-            state.recommendations = payload;
-        },
-        setRecommendationLoading(state, payload) {
-            state.recommendationLoading = payload;
-        }
-    },
-    getters: {
-        isLoading(state) {
-            return state.loading;
-        },
-        isRecommendationLoading(state) {
-            return state.recommendationLoading;
-        }
-    }
-});
 
 app.use(store);
 
