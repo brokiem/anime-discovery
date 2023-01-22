bc5d283da360ecdac6ec5c6d67b86018d908203f
brokiem <milyhqvideo@gmail.com>
2023-01-22 21:16:26 +0800

    Add page skeleton loading

--- app/anime/[title]/loading.tsx
+++ app/anime/[title]/loading.tsx
@@ -1,7 +1,45 @@
+"use client";
+
 export default function Loading() {
     return (
         <>
-            <h1>Loading...</h1>
+            <div className="flex content-center justify-center">
+                <div className="mt-10 w-full max-w-5xl text-white">
+                    <div className="mx-5">
+                        <div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 md:flex md:items-center">
+                            <div className="flex items-center justify-center w-full h-80 bg-gray-300 rounded md:w-80 dark:bg-grey">
+                                <svg className="w-12 h-12 text-gray-400" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" fill="currentColor" viewBox="0 0 640 512">
+                                    <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z"/>
+                                </svg>
+                            </div>
+
+                            <div className="w-full">
+                                <div className="h-5 bg-gray-200 rounded-full dark:bg-grey w-80 mb-4"></div>
+
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[580px] mb-2.5 mt-8"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey mb-2.5 max-w-[590px]"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[540px] mb-2.5"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[570px] mb-2.5"></div>
+
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[560px] mb-2.5 mt-8"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[550px] mb-2.5"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[580px] mb-2.5"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey mb-2.5 max-w-[590px]"></div>
+
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[540px] mb-2.5 mt-8"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[560px] mb-2.5"></div>
+                                <div className="h-2 bg-gray-200 rounded-full dark:bg-grey max-w-[460px]"></div>
+                            </div>
+
+                            <span className="sr-only">Loading...</span>
+                        </div>
+
+                        <hr className="animate-pulse rounded my-5"/>
+
+                        <div className="animate-pulse h-40 bg-gray-200 rounded-md dark:bg-grey mb-2.5 w-full"></div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
