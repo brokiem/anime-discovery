d496a8ecf1b6209939b3afde4df9091dc13a2b1c
brokiem <milyhqvideo@gmail.com>
2023-01-22 22:37:15 +0800

    Darken home button when hover

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap mb-4 gap-x-2">
     <div>
       <a href="/anime-discovery/">
-        <button type="button" class="outline-none ring-0 ml-5 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 font-medium rounded-md px-3 py-1.5 dark:bg-grey dark:text-white dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:border-gray-600 dark:focus:ring-gray-700 transition duration-200">
+        <button type="button" class="outline-none ring-0 ml-5 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 font-medium rounded-md px-3 py-1.5 dark:bg-grey dark:text-white dark:border-gray-600 dark:hover:bg-[#282828] dark:hover:border-gray-600 dark:focus:ring-gray-700 transition duration-200">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
           </svg>
